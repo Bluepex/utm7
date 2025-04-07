Bluepex/utm7

commit 3385d7fc7ef6306eb531aaeeb5fdf0d94aa931bc

Remove unnecessary case statement since tar now uses libarchive and handles the different types of compression automatically

src/etc/rc.update_bogons.sh
@@ -38,7 +38,6 @@ process_url() {
 	local file=$1
 	local url=$2
 	local filename=${url##*/}
-	local ext=${filename#*.}
 
 	/usr/bin/fetch -a -w 600 -T 30 -q -o $file "${url}"
 
@@ -47,26 +46,7 @@ process_url() {
 		proc_error="true"
 	fi
 
-	case "$ext" in
-		tar)
-			mv $file $file.tmp
-			/usr/bin/tar -xf $file.tmp -O > $file 2> /dev/null
-			;;
-		tar.gz)
-			mv $file $file.tmp
-			/usr/bin/tar -xzf $file.tmp -O > $file 2> /dev/null
-			;;
-		tgz)
-			mv $file $file.tmp
-			/usr/bin/tar -xzf $file.tmp -O > $file 2> /dev/null
-			;;
-		tar.bz2)
-			mv $file $file.tmp
-			/usr/bin/tar -xjf $file.tmp -O > $file 2> /dev/null
-			;;
-		*)
-			;;
-	esac
+	/usr/bin/tar -xf "$file.tmp" -O > "$file" 2> /dev/null
 
 	if [ -f $file.tmp ]; then
 		rm $file.tmp

commit 80e65d6f11dc92bc52ec26075161f6029910f9c8

Check for expected file extension before extracting the file. Fix #16129
This regressed with 3385d7fc7ef6306eb531aaeeb5fdf0d94aa931bc.

src/etc/rc.update_bogons.sh
@@ -38,6 +38,7 @@ process_url() {
 	local file="$1"
 	local url="$2"
 	local filename="${url##*/}"
+	local ext=${filename#*.}
 
 	/usr/bin/fetch -a -w 600 -T 30 -q -o "$file" "${url}"
 
@@ -46,7 +47,14 @@ process_url() {
 		proc_error="true"
 	fi
 
-	/usr/bin/tar -xf "$file.tmp" -O > "$file" 2> /dev/null
+	case "$ext" in
+		tar|tar.gz|tgz|tar.bz2)
+			mv "$file" "$file.tmp"
+			/usr/bin/tar -xf "$file.tmp" -O > "$file" 2> /dev/null
+			;;
+		*)
+			;;
+	esac
 
 	if [ -f "$file.tmp" ]; then
 		rm "$file.tmp"